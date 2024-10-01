84af30cc0f235f3522e0b23805b72562aef7ac33
BeldyAdam <beldy.adam@gmail.com>
2024-10-01 15:08:26 +0200

    css szerkeztve

--- adatok/adatok.js
+++ adatok/adatok.js
@@ -22,5 +22,29 @@ export const adatLista = [
         kep : "adatok/kepek/4.jpg",
         motor : "Diesel",
         id : "4"
+    },
+    {
+        nev : "Auto1",
+        kep : "adatok/kepek/1.jpg",
+        motor : "Hybrid",
+        id : "1"
+    },
+    {
+        nev : "Auto2",
+        kep : "adatok/kepek/2.jpg",
+        motor : "Elektromos",
+        id : "2"
+    },
+    {
+        nev : "Auto3",
+        kep : "adatok/kepek/3.jpg",
+        motor : "Benzin",
+        id : "3"
+    },
+    {
+        nev : "Auto4",
+        kep : "adatok/kepek/4.jpg",
+        motor : "Diesel",
+        id : "4"
     }
 ];
